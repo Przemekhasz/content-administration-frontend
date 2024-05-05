eadc96a7f2d01c9e0700a6232caf647e3b2f3087
Przemekhash <tarapackiprzemek45@gmail.com>
2024-05-05 23:09:34 +0200

    body text, footer

--- src/Infrastructure/Shared/components/LoadingScreen.tsx
+++ src/Infrastructure/Shared/components/LoadingScreen.tsx
@@ -1,22 +1,54 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
+import ExploreIcon from '@mui/icons-material/Explore';
 
-const LoadingScreen: React.FC = () => {
+const LoadingComponent = () => {
     return (
-        <Box display="flex" alignItems="center" justifyContent="center" height="150vh" flexDirection="column">
-            <CameraIcon style={{ fontSize: 60, animation: "spin 2s linear infinite" }} />
-            <Typography variant="h6" marginTop={2}>
-                Loading...
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            height="100vh"
+            flexDirection="column"
+            style={{ animation: 'fadeIn 0.5s' }}
+        >
+            <ExploreIcon
+                style={{
+                    fontSize: 60,
+                    animation: "spin 2s linear infinite",
+                    transform: "translateZ(0)"  // Sprzętowa akceleracja
+                }}
+            />
+            <Typography variant="h6" marginTop={2} style={{ position: 'relative', width: 'fit-content' }}>
+                <span style={{ whiteSpace: 'pre' }}>
+                    L<span style={{ animation: 'typing 2s steps(7, end) infinite' }}>oading...</span>
+                </span>
             </Typography>
-            <style>{`
+            <style>
+                {`
                 @keyframes spin {
                     0% { transform: rotate(0deg); }
                     100% { transform: rotate(360deg); }
                 }
-            `}</style>
+
+                @keyframes fadeIn {
+                    from { opacity: 0; }
+                    to { opacity: 1; }
+                }
+
+                @keyframes typing {
+                    from { width: 0; }
+                    to { width: 100%; }
+                }
+
+                @keyframes blink {
+                    from, to { border-color: transparent; }
+                    50% { border-color: black; }
+                }
+                `}
+            </style>
         </Box>
     );
 };
 
-export default LoadingScreen;
+export default LoadingComponent;
