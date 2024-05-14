c081818d8540849c61a7c5b8fa24e6f2bd107c06
Przemekhash <tarapackiprzemek45@gmail.com>
2024-05-14 18:11:18 +0200

    useless imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import i18n from "i18next";
 
 ReactDOM.render(
     <React.StrictMode>
